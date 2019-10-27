e01ae02c3b52911b8bdbf8c0026bd1d5caa1bbe0
moflash <eddyed.k@gmail.com>
2019-10-27 11:01:43 +0100

    CCS7.4 project settup

---
+++ CCS_7.4_workspace/reflow/HAL/hal_gpio.h
@@ -0,0 +1,18 @@
+/*
+ * hal_gpio.h
+ *
+ *  Created on: Oct 27, 2019
+ *      Author: Edwin Koch
+ */
+
+#ifndef HAL_HAL_GPIO_H_
+#define HAL_HAL_GPIO_H_
+
+#include "../HAL/hal_gpio.h"
+
+void hal_gpio_init();
+
+void hal_gpio_
+
+
+#endif /* HAL_HAL_GPIO_H_ */
---
+++ CCS_7.4_workspace/reflow/sys_settings.h
@@ -0,0 +1,15 @@
+/*
+ * sys_settings.h
+ *
+ *  Created on: Oct 27, 2019
+ *      Author: Edwin
+ */
+
+#ifndef SYS_SETTINGS_H_
+#define SYS_SETTINGS_H_
+
+
+
+
+
+#endif /* SYS_SETTINGS_H_ */
